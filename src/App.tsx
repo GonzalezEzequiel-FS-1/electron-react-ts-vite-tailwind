cfe13a9fdbf0246507678a7f74749b3832b49274
Ra1NuX <ra1nux.01@gmail.com>
2021-11-03 14:34:30 +0100

    light-theme add

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,60 @@
 import { useState, useEffect } from 'react';
-import './default.css'
+import pkg from '../package.json'
+import { IoMoon, IoSunny } from 'react-icons/io5';
+import {BsMoonStarsFill, BsSunFill} from 'react-icons/bs'
+import {VscChromeMaximize} from 'react-icons/vsc'
+import './default.css';
+import "./lightmode.css"
+
 
 declare global {
   interface Window{handler:any}
 }
 
-function App() {
 
+function App() {
   const { onClose, onMinimize, onMaximize, isFullScreen } = window.handler;
   const [FullScreen, setFullScreen] = useState(false);
+  const [isLight, setIsLight] = useState(false)
 
   useEffect(()=> {
     setFullScreen(isFullScreen())
   },[])
-  window.addEventListener('resize', () => {setFullScreen(isFullScreen())})
 
+  window.addEventListener('resize', () => {setFullScreen(isFullScreen())})
+  const handleLight = () => {
+  console.log(isLight)
+  setIsLight(!isLight)
+  }
+  const fs = () => {
+    return FullScreen ? "contentFS" : "content" 
+  }
+  const light = () => {
+    return isLight ? "light" : ""
+  }
+  
   const titleBar = () => {
-    if (!FullScreen) {
-      return <div id="title-bar">
-        <div id="title">Title</div>
+    if (!FullScreen){
+      return <div id="title-bar" className={light()+'bar'}>
+        <div id="title" className={light()+'bar'}>{pkg.build.productName}</div>
         <div id="title-bar-btns">
-          <button id="min-btn" onClick={() => { onMinimize() }}></button>
-          <button id="max-btn" onClick={() => { onMaximize() }}></button>
-          <button id="close-btn" onClick={() => { onClose() }}></button>
+          <button className="lightbtn" style={isLight ? {color: "black"} : {color: "white"}} onClick={() => {handleLight()}}>
+            <span>{!isLight ? <BsSunFill/> : <BsMoonStarsFill/>}</span>
+          </button>
+          <button id="min-btn" style={isLight ? {color: "black"} : {color: "white"}} onClick={() => { onMinimize() }}><span>-</span></button>
+          <button id="max-btn" style={isLight ? {color: "black"} : {color: "white"}} onClick={() => { onMaximize() }}><span id="maximize"><VscChromeMaximize/></span></button>
+          <button id="close-btn" style={isLight ? {color: "black"} : {color: "white"}} onClick={() => { onClose() }}><span>&times;</span></button>
         </div>
       </div>
     } else { return <></> }
   }
-  return (<>
+
+  return (<div className={light()} id="main">
     {titleBar()}
-    <div id="content">
-      Here You can write HERE your content
+    <div className={fs() +" "+ light()}>
+      Here you can write your content
     </div>
-  </>)
+  </div>)
 }
 
 export default App
