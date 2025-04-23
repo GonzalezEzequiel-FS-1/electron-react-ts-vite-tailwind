66a2e47412199aed8491c8020db40ebebd840869
3myr <remy.demazieres.store@gmail.com>
2025-04-23 11:34:29 +0200

    fix: correct URL generation in getURL to avoid path.join on URLs

--- main/lib/getUrl.ts
+++ main/lib/getUrl.ts
@@ -1,9 +1,8 @@
 
-import path from 'path'
 import isDev from './isDev'
 
 export const domain = isDev
     ? `http://localhost:5173`
     : 'app://example.com/'
 
-export const getURL = (pathname: string) => path.join(domain, pathname)
+export const getURL = (pathname: string) => new URL(pathname, domain).toString()
